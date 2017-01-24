cdcc26779680608050b1d0df8e510fb431fe579e
Niesha <nieshaamos@gmail.com>
2017-01-24 14:28:32 -0500

    Adding 'take a break' python program

--- break_time.py
+++ break_time.py
@@ -6,5 +6,6 @@ i = 1
 
 while i < 4:
     time.sleep(10)
-    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    #open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
     i = i+1
