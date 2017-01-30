dc8ffd77a044d870572175e1c76ff6b05a7492a7
Niesha <nieshaamos@gmail.com>
2017-01-30 14:35:31 -0500

    commit Twilio starter application

--- send_sms.py
+++ send_sms.py
@@ -1,12 +1,12 @@
 from twilio.rest import TwilioRestClient
 
-account_sid = "{{ account_sid }}" # Your Account SID from www.twilio.com/console
-auth_token  = "{{ auth_token }}"  # Your Auth Token from www.twilio.com/console
+account_sid = "ACfc0a5a5d2c7827b9992cfb635c75067e" # Your Account SID from www.twilio.com/console
+auth_token  = "b8d87fdca9b800d422fe0ba2891cd80a"  # Your Auth Token from www.twilio.com/console
 
 client = TwilioRestClient(account_sid, auth_token)
 
 message = client.messages.create(body="Hello from Python",
-    to="+12345678901",    # Replace with your phone number
-    from_="+12345678901") # Replace with your Twilio number
+    to="+13139718814",    # Replace with your phone number
+    from_="+13133273089") # Replace with your Twilio number
 
 print(message.sid)
